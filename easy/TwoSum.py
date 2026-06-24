e0bc8e1d490d0d5649c884a1c0c879a35e7d568e
Angel Velasquez <avelasquez7711@gmail.com>
2026-06-24 12:34:32 -0700

    Added Time Complexity details
    - Added Time Complexity details
    - Fixed typo: "compliment" --> "complement"

--- easy/TwoSum.py
+++ easy/TwoSum.py
@@ -1,9 +1,14 @@
 ''' 
 This is my solution for the Two Sum problem on Leetcode. 
+
+Complexity:
+    Time Complexity: O(n) - Single pass through the list with O(1) hash map lookups.
+    Space Complexity: O(n) - Storing up to n elements in the dictionary.
+
 Problem: 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
 '''
 
 class Solution:
@@ -12,11 +17,11 @@ class Solution:
         num_to_index = {}  
 
         for index, num in enumerate(nums):
-            compliment = target - num 
+            complement = target - num 
 
             # check if matching pair needed for target was processed
-            if compliment in num_to_index:
-                return [num_to_index[compliment], index]
+            if complement in num_to_index:
+                return [num_to_index[complement], index]
             else:
                 num_to_index[num] = index  # save current number to index
 
